dbb17e575b4e5841378452aca337d5707bf44383
alef90 <thommy90vercetti@gmail.com>
2021-04-12 06:42:27 -0300

    add exercicioLinkedListQueue

--- src/main/java/exemplosAulas/exercicioArrayList.java
+++ src/main/java/exemplosAulas/exercicioArrayList.java
@@ -12,53 +12,53 @@ public class exercicioArrayList {
         nomes.add("Carlos");
         nomes.add("Larissa");
         nomes.add("João");
-        System.out.println("Lista de nomes: "+nomes);
+        System.out.println("1 - Lista de nomes: "+nomes);
 
         nomes.set(2, "Roberto");
-        System.out.println("Substituindo Carlos por Roberto: "+nomes);
+        System.out.println("2 - Substituindo Carlos por Roberto: "+nomes);
 
-        System.out.println("Exibindo o nome na posição '1': "+nomes.get(1));
+        System.out.println("3 - Exibindo o nome na posição '1': "+nomes.get(1));
 
         nomes.remove(4);
-        System.out.println("Removendo a posição '4': "+nomes);
+        System.out.println("5 - Removendo a posição '4': "+nomes);
 
         if (nomes.contains("João")) {
             nomes.remove("João");
-            System.out.println("Exibindo lista de nomes sem o João: "+nomes);
+            System.out.println("6 - Exibindo lista de nomes sem o João: "+nomes);
         } else {
-            System.out.println("João já foi removido!");
+            System.out.println("6 - João já foi removido!");
         }
 
-        System.out.println("Exibindo a quantidade de itens da lista de nomes: "+nomes.size());
+        System.out.println("7 - Exibindo a quantidade de itens da lista de nomes: "+nomes.size());
 
         if (nomes.contains("Juliano")) {
-            System.out.println("Juliano esta na lista!");
+            System.out.println("8 - Juliano esta na lista!");
         } else {
-            System.out.println("Juliano não esta na lista!");
+            System.out.println("8 - Juliano não esta na lista!");
         }
 
         List<String> nomes2 = new ArrayList<>();
         nomes2.add("Ismael");
         nomes2.add("Rodrigo");
-        System.out.println("Segunda lista de nomes: "+nomes2);
+        System.out.println("9 - Segunda lista de nomes: "+nomes2);
 
         nomes.addAll(nomes2);
-        System.out.println("Nova lista de nomes: "+nomes);
+        System.out.println("10 - Nova lista de nomes: "+nomes);
 
         Collections.sort(nomes);
-        System.out.println("Ordem alfabética da lista de nomes: "+nomes);
+        System.out.println("11 - Ordem alfabética da lista de nomes: "+nomes);
 
         if (nomes.isEmpty()) {
-            System.out.println("A lista de nomes esta vazia!");
+            System.out.println("12 - A lista de nomes esta vazia!");
         } else {
-            System.out.println("A lista de nomes não esta vazia, exibindo a lista: "+nomes);
+            System.out.println("12 - A lista de nomes não esta vazia, exibindo a lista: "+nomes);
         }
 
         nomes2.clear();
         if (nomes2.isEmpty()) {
-            System.out.println("A segunda lista de nomes esta vazia!");
+            System.out.println("13 - A segunda lista de nomes esta vazia!");
         } else {
-            System.out.println("Segunda lista de nomes não esta vazia, exibindo a lista: "+nomes2);
+            System.out.println("13 - Segunda lista de nomes não esta vazia, exibindo a lista: "+nomes2);
         }
     }
 }
