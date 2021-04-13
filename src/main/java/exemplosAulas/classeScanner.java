1f468b11ffd34b6ce903f9b50c23ac8db50cca84
alef90 <thommy90vercetti@gmail.com>
2021-04-13 07:33:32 -0300

    add exercicioCollectionHashSet, exercicioCollectionLinkedHashSet, exercicioCollectionTreeSet

--- src/main/java/exemplosAulas/classeScanner.java
+++ src/main/java/exemplosAulas/classeScanner.java
@@ -1,23 +1,17 @@
 package exemplosAulas;
-//import java.lang.Exception;
 import java.util.Scanner;
 public class classeScanner {
     public static void main(String[]args) {
         Scanner text = new Scanner(System.in);
-        //String str;
 
-        //try {
             System.out.println("Enter your name: ");
             String name = text.nextLine();
             System.out.println("Wellcome " + name);
-        //} catch (Exception one) { System.out.println("Type correctly!"); }
 
-        //try {
             System.out.println("Imput a number: ");
             int numero = text.nextInt();
             System.out.println("The number entered was: " + numero);
-        //} catch (Exception two) { System.out.println("Type correctly!"); }
-        
+
         text.close();
     }
 }
---
+++ src/main/java/exemplosAulas/exercicioCollectionTreeSet.java
@@ -0,0 +1,14 @@
+package exemplosAulas;
+
+public class exercicioCollectionTreeSet {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*Adicione 5 numeros inteiros: 3, 88, 20, 44, 3.
+ * Navegue no Set exibindo cada numero no console.
+ * Remova o primeiro item do Set.
+ * Adicione um novo numero no Set: 23.
+ * Verifique o tamanho do Set.
+ * Verifique se o Set está vazio.*/
