fa4fbf03183989ce1bf772db2adcd5650344c2f3
alef90 <thommy90vercetti@gmail.com>
2021-04-21 06:05:14 -0300

    add exemploDeOptionals

---
+++ src/main/java/optionalExemplo/OptionalExemplo.java
@@ -0,0 +1,25 @@
+package optionalExemplo;
+
+import java.util.Optional;
+
+public class OptionalExemplo {
+    public static void main(String[] args) {
+
+        Optional<String> optionalString = Optional.of("\n valor presente");
+        System.out.println(" valor opcional que esta presente");
+        optionalString.ifPresentOrElse(System.out::println, () -> System.out.println(" não está presente"));
+
+        Optional<String> optionalNull = Optional.ofNullable(null);
+        System.out.println("\n valor opcional que não esta presente");
+        optionalNull.ifPresentOrElse(System.out::println, () -> System.out.println(" null = não esta presente"));
+
+        Optional<String> emptyOptional = Optional.empty();
+        System.out.println("\n valor opcional que não esta presente");
+        emptyOptional.ifPresentOrElse(System.out::println, () -> System.out.println(" empty = não esta presente"));
+
+        Optional<String> optionalNullErro = Optional.of(" null");
+        System.out.println("\n valor opcional que lança erro NullPointerException");
+        optionalNullErro.ifPresentOrElse(System.out::println, () -> System.out.println(" erro = não esta presente"));
+
+    }
+}
