0cb9268260debdf977c06019c8674c3a56ae90e9
alef90 <thommy90vercetti@gmail.com>
2021-04-21 09:44:18 -0300

    add ExercicioOptionals

---
+++ src/main/java/optionalExercicios/optionalExercicio.java
@@ -0,0 +1,27 @@
+package optionalExercicios;
+import java.util.Optional;
+public class optionalExercicio {
+    public static void main(String[] args) {
+
+        Optional<Integer> optionalInteger = Optional.of(75);
+        optionalInteger.ifPresent(System.out::println);
+
+//        Optional<Integer> optionalInteger2 = Optional.empty();
+//        if (Optional.empty().isEmpty()){
+//            System.out.println("tes"+IllegalStateException);
+//        } else {
+//            System.out.println(optionalInteger2);
+//        }
+
+
+    }
+}
+
+/*Crie um optional de um determinado tipo de dado.--
+* Crie com estado vazio, estado presente, e com null.--
+* Se vazio, lançe uma exceção IlegalStateException.
+* Se presente, exiba o valor no console.
+* Se vazio, exiba "Optional vazio" no console.
+* Se presente, transforme o valor e exiba no console.
+* Se presente, pegue o valor do optional e atribua em uma variavel.
+* Se presente, filtre o optional com uma determinada regra de negocio.*/
