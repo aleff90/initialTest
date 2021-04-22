8fa32d7de8d6d5a7b0862326b8b16b630e2ffbec
alef90 <thommy90vercetti@gmail.com>
2021-04-22 11:47:58 -0300

    refactor

--- src/main/java/exemplosAulas/optionalExercicio.java
+++ src/main/java/exemplosAulas/optionalExercicio.java
@@ -1,16 +1,21 @@
 package exemplosAulas;
+
 import java.util.Optional;
+import java.util.OptionalDouble;
+
 public class optionalExercicio {
+
     public static void main(String[] args) {
 
         Optional<Integer> optionalInteger = Optional.of(75);
-        optionalInteger.ifPresent(System.out::println);
+        optionalInteger.ifPresentOrElse(System.out::println, ()-> System.out.println("\n não esta presente"));
 
         Optional<String> optionalString = Optional.empty();
-        optionalString.ifPresentOrElse(System.out::println, () -> System.out.println("\n esta vazio"));
+        optionalString.ifPresentOrElse(System.out::println, () -> System.out.println("\n não esta vazio"));
 
-        Optional<Double> optionalDouble = Optional.ofNullable(null);
-        optionalDouble.ifPresentOrElse(System.out::println, () -> System.out.println("\n null esta presente"));
+        Optional<Integer> optionalInteger2 = Optional.of(987987);
+        System.out.println("\n exibindo optional integer2: "+optionalInteger2);
+
     }
 }
 
@@ -22,3 +27,7 @@ public class optionalExercicio {
 * Se presente, transforme o valor e exiba no console.
 * Se presente, pegue o valor do optional e atribua em uma variavel.
 * Se presente, filtre o optional com uma determinada regra de negocio.*/
+
+//Optional<Double> optionalDouble = Optional.ofNullable(null);
+//optionalDouble.ifPresentOrElse(System.out::println, () -> System.out.println("\n null esta presente"));
+
