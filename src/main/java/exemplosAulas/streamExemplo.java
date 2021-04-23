117711abd393abeb7007019e528619ee760c2393
alef90 <thommy90vercetti@gmail.com>
2021-04-23 12:20:57 -0300

    add streamExemplo

---
+++ src/main/java/exemplosAulas/streamExemplo.java
@@ -0,0 +1,24 @@
+package exemplosAulas;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class streamExemplo {
+    public static void main(String[] args) {
+
+        List<String> estudantes = new ArrayList<>();
+
+        estudantes.add("Pedro");
+        estudantes.add("Thayse");
+        estudantes.add("Marcelo");
+        estudantes.add("Carla");
+        estudantes.add("Juliana");
+        estudantes.add("Thiago");
+        estudantes.add("Rafael");
+
+        //retorna
+
+    }
+}
