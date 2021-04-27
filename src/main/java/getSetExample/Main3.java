4019b7c5cf2a1deb525fd6f0c0f63d24866febfc
alef90 <thommy90vercetti@gmail.com>
2021-04-27 06:56:25 -0300

    add getSetExample

---
+++ src/main/java/getSetExample/Main3.java
@@ -0,0 +1,9 @@
+package getSetExample;
+
+public class Main3 {
+    public static void main(String[] args) {
+        Person myObj = new Person();
+        myObj.name = "John"; //error while String name stay private
+        System.out.println(myObj.name); //error while String name stay private
+    }
+}
