49ce3b498c9c659e631bdb3ee1a75ede43531c03
alef90 <thommy90vercetti@gmail.com>
2021-05-07 07:51:34 -0300

    add variableStoreValue package

---
+++ src/main/java/variableStoreValue/Main.java
@@ -0,0 +1,11 @@
+package variableStoreValue;
+
+public class Main {
+    static int myMethod(int x, int y){
+        return  x+y;
+    }
+    public static void main(String[] args) {
+        int z = myMethod(5,3);
+        System.out.println("\n "+z);
+    }
+}
