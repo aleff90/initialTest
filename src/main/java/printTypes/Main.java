096a86a00bdc94b3cb84265663c223997110ba8c
alef90 <thommy90vercetti@gmail.com>
2021-05-07 09:01:52 -0300

    add printTypes package

---
+++ src/main/java/printTypes/Main.java
@@ -0,0 +1,9 @@
+package printTypes;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print(" |este aqui é o print, não salta linha| ");
+        System.out.println(" |este aqui é o println, salta uma linha| ");
+        System.out.printf("%s%d%s", " |testando o printf| ", 23, " |escreve texto com formatação|");
+    }
+}
