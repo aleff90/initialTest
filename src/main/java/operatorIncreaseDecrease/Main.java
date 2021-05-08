1e464382945bb290b23450da1e101c85769530f5
alef90 <thommy90vercetti@gmail.com>
2021-05-08 11:39:14 -0300

    add operatorIncreaseDecrease package

---
+++ src/main/java/operatorIncreaseDecrease/Main.java
@@ -0,0 +1,11 @@
+package operatorIncreaseDecrease;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero++;
+        System.out.println("\n Variavel numero com valor 5 incrementa mais 1: "+numero);
+        numero--;
+        System.out.println("\n Variavel numero com valor 6 decrementa menos 1: "+numero);
+    }
+}
