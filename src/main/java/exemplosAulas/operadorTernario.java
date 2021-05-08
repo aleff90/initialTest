d33d03d757bc290456b9f3bee45ed8fb47d3b991
alef90 <thommy90vercetti@gmail.com>
2021-05-08 11:17:54 -0300

    add operadorTernario package

--- src/main/java/exemplosAulas/operadorTernario.java
+++
@@ -1,9 +0,0 @@
-package exemplosAulas;
-
-public class operadorTernario {
-    public static void main(String[]args) {
-        int num1=2, num2=3, quinzena=12;
-        System.out.println((num1==num2)?"verdadeiro":"falso");
-        System.out.println((quinzena<15)?"\nprimeira quinzena.":"segunda quinzena.");
-    }
-}
---
+++ src/main/java/operadorTernario/Main.java
@@ -0,0 +1,9 @@
+package operadorTernario;
+
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 215264847, num2 = 748884467, quinzena = 12;
+        System.out.println((num1==num2)?"\n Verdadeiro":"\n Falso");
+        System.out.println((quinzena<15)?"\n Primeira quinzena":"\n Segunda quinzena");
+    }
+}
