925a1275584b97bf93d5276a4ae89d586c3af58e
alef90 <thommy90vercetti@gmail.com>
2021-05-09 08:58:15 -0300

    add forExample package

--- src/main/java/exemplosAulas/forExemplo.java
+++
@@ -1,9 +0,0 @@
-package exemplosAulas;
-public class forExemplo {
-     public static void main(String[]args) {
-        for (int a=0; a<4; a++)
-           System.out.println("lista 1: "+a);
-        for (int b=0; b<=4; b++)
-           System.out.println("lista 2: "+b);
-    }
-}
---
+++ src/main/java/forExample/Main.java
@@ -0,0 +1,18 @@
+package forExample;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int a=0;a<=5;a++){
+            System.out.println(" a = "+a);
+        }
+        for (double b=6.4;b<=7.3;b++){
+            System.out.println(" b = "+b);
+        }
+        for (long c=587473625;c<587473635;c++){
+            System.out.println(" c = "+c);
+        }
+        //for (float d=654872139;d<=654872140;d++){
+        //    System.out.println(" d = "+d);
+        //}
+    }
+}
