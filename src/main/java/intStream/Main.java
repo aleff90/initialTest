9c5c85f5c99a1042093759e2484a0aff8932ba05
alef90 <thommy90vercetti@gmail.com>
2021-05-10 06:15:08 -0300

    add intStream package

--- src/main/java/intStream/Main.java
+++ src/main/java/intStream/Main.java
@@ -1,11 +1,9 @@
-package exemplosAulas;
-
+package intStream;
 import java.util.stream.IntStream;
-
-public class intStreams {
+public class Main {
     public static void main(String[] args) {
         IntStream.of(1,2,3,4,5).forEach(System.out::println);
 
-        IntStream.rangeClosed(0,3).forEach(n-> System.out.println("Numero = "+n));
+        IntStream.rangeClosed(0,3).forEach(n->System.out.println(" n = "+n));
     }
 }
