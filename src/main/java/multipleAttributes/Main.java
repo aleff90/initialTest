6506a8859317fcbdc65ad364a003eced0adcf8ee
alef90 <thommy90vercetti@gmail.com>
2021-05-10 06:55:19 -0300

    add multipleAttributes package

---
+++ src/main/java/multipleAttributes/Main.java
@@ -0,0 +1,13 @@
+package multipleAttributes;
+
+public class Main {
+    String fname = "Svetlana";
+    String lname = "Pavlov";
+    int age = 45;
+
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println("\n Name: "+myObj.fname+" "+myObj.lname);
+        System.out.println("\n Age: "+myObj.age);
+    }
+}
